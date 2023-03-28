9a6e1985f5039e1c6062b85767552f1e13b74cff
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-03-28 20:54:10 +0200

    Add navbar and center the form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,10 +4,13 @@
   import Input from '../../components/FormComponents/Input.svelte';
   import SubmitButton from '../../components/FormComponents/SubmitButton.svelte';
   import { enhance } from '$app/forms';
+  import Navbar from '../../components/Navbar.svelte';
 </script>
 
+<Navbar />
+
 <form method="post" id="form">
-  <div class="space-y-6 px-4 py-8 sm:px-8 md:px-16 lg:px-32 xl:px-48 2xl:px-72">
+  <div class="absolute left-1/2 top-1/2 w-full max-w-[100rem] -translate-x-1/2 -translate-y-1/2 space-y-6 px-4 py-8 sm:px-8 md:px-16 lg:px-32 xl:px-48 2xl:px-72">
     <FormQuestionContainer title="Staff Login" description="Only staff members may login">
       <Input label="Username" placeholder="Craig" name="username" size="short" type="text" />
       <Input label="Password" placeholder="····" name="password" size="short" type="password" />
