7b384efacb934fbde038359c1fcc72c7cfe374f7
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-03-28 20:53:30 +0200

    Empty space for placeholder to work with better error display

--- src/components/FormComponents/TextArea.svelte
+++ src/components/FormComponents/TextArea.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let label = 'Undefined';
-  export let placeholder = 'Undefined';
+  export let placeholder = ' ';
   export let name = 'Undefined';
   export let size = 'medium';
   export let constraints: any;
@@ -23,6 +23,14 @@
 <div class={size}>
   <label for={name} class="block text-sm font-medium text-neutral-200">{label}</label>
   <div class="mt-1">
-    <textarea required {placeholder} {...constraints} rows="4" {name} id={name} class=" mt-1 block max-h-56 min-h-[98px] w-full resize-y rounded-md border-neutral-800 bg-black text-white placeholder-white placeholder-opacity-40 shadow-sm hover:border-neutral-500 focus:border-neutral-500 focus:ring-transparent focus:invalid:border-red-800 sm:text-sm" />
+    <textarea required {placeholder} {...constraints} rows="4" {name} id={name} class=" mt-1 block max-h-56 min-h-[98px] w-full resize-y rounded-md border-neutral-800 bg-black text-white placeholder-white placeholder-opacity-40 shadow-sm hover:border-neutral-500 focus:border-neutral-500 focus:ring-transparent sm:text-sm" />
   </div>
 </div>
+
+<style>
+  textarea:not(:focus):not(:placeholder-shown):invalid,
+  textarea:not(:placeholder-shown):invalid {
+    --tw-border-opacity: 1;
+    border-color: rgb(153 27 27 / var(--tw-border-opacity));
+  }
+</style>
