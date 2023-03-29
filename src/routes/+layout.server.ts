5f6555474f583a6d72a014f7dc6bcd40f15b2d08
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-03-29 23:10:49 +0200

    Fix POJO error

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,9 +5,9 @@ import type { User } from '$lib/database/models/user';
 export const load: LayoutServerLoad = async ({ cookies }) => {
   const resp = await validateSession(cookies);
   if (resp == null) {
-    return { };
+    return {};
   }
   // devalue doesn't like "_id"
-  resp.user.password = "";
-  return { user: (resp.user as User) };
+  resp.user.password = '';
+  return { user: structuredClone(resp.user as User) };
 };
