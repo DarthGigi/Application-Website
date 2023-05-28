69a7fd790905466afe6a65e7e7fdcaf4bb83c070
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-05-28 20:36:19 +0200

    Add application submition notification

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 import type { Actions } from './$types';
 import { fail, redirect } from '@sveltejs/kit';
-import { z } from 'zod';
+import * as z from 'zod';
 import { superValidate } from 'sveltekit-superforms/server';
 import Applications from '$lib/server/database/models/application';
 import { ApplicationStatus, type FormResponses, type FormAgreements } from '$lib/types/application';
@@ -10,26 +10,9 @@ import mongoose from 'mongoose';
 import type { PageServerLoad } from './$types';
 import type { Application } from '$lib/types/application';
 import { validateSession } from '$lib/server/auth';
+import { DISCORD_BOT_TOKEN } from '$env/static/private';
 
 const botDetect = new RegExp('/(bot)/gm');
-
-const formSchema = z.object({
-  name: z.string().min(1),
-  siriusDiscovery: z.string().min(1),
-  spareTime: z.string().min(1),
-  question1: z.string().min(1),
-  question2: z.string().min(1),
-  question3: z.string().min(1),
-  question4: z.string().min(1),
-  question5: z.string().min(1),
-  question6: z.string().min(1),
-  question7: z.string().min(1),
-  question8: z.string().min(1),
-  question9: z.string().min(1),
-  contactStaff: z.string().min(2),
-  contactInfo: z.string().min(2),
-  data: z.string().min(2)
-});
 
 export const load = (async (event) => {
   const sess = await validateSession(event.cookies);
@@ -52,9 +35,8 @@ export const actions: Actions = {
     const session = await validateSession(event.cookies);
 
     if (!session) throw redirect(302, '/');
-
     // Validate the form itself
-    const form = await superValidate(await event.request.clone().formData(), formSchema);
+    const form = await superValidate(await event.request.clone().formData(), z.object({ name: z.string().min(1), siriusDiscovery: z.string().min(1), spareTime: z.string().min(1), question1: z.string().min(1), question2: z.string().min(1), question3: z.string().min(1), question4: z.string().min(1), question5: z.string().min(1), question6: z.string().min(1), question7: z.string().min(1), question8: z.string().min(1), question9: z.string().min(1), contactStaff: z.string().min(2), contactInfo: z.string().min(2), data: z.string().min(2) }));
 
     if (!form.valid) return fail(400, { form });
 
@@ -105,10 +87,71 @@ export const actions: Actions = {
       discord: session.user.discord
     };
 
-    await new Applications(app).save();
+    const application = await new Applications(app).save();
+    if (application._id) {
+      try {
+        const embed = {
+          embeds: [
+            {
+              title: 'New Application',
+              description: `**${await application.discord.User.username}** has submitted an application!`,
+              fields: [
+                {
+                  name: 'Name',
+                  value: `${await application.name}`,
+                  inline: true
+                },
+                {
+                  name: 'Discord ID',
+                  value: `\`${await application.discord.User.id}\``,
+                  inline: true
+                },
+                {
+                  name: 'Discord Account',
+                  value: `<@${await application.discord.User.id}>`,
+                  inline: true
+                }
+              ],
+              color: 2829617,
+              author: {
+                name: '‎',
+                icon_url: `${await application.discord.User.avatar}`
+              }
+            }
+          ],
+          components: [
+            {
+              type: 1,
+              components: [
+                {
+                  type: 2,
+                  label: 'View Application',
+                  style: 5,
+                  url: `https://apply.sirius.menu/dashboard/${await application._id}`
+                }
+              ]
+            }
+          ]
+        };
 
+        const res = await fetch('https://discord.com/api/v10/channels/1092822492604796938/messages', {
+          method: 'POST',
+          headers: {
+            Authorization: `Bot ${DISCORD_BOT_TOKEN}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(embed)
+        });
+
+        if (!res.ok) {
+          console.error(await res.text());
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
     return {
-      form
+      props: form
     };
   }
 };
